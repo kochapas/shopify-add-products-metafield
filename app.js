ae0f7945ca68ba9d54b2b8b0468c64baba4731da
Kochapas Santibut <kochapas@outlook.com>
2023-04-08 02:11:08 +0900

    Add queries to read products data. Update ENV format.

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 import * as dotenv from "dotenv";
-dotenv.config()
+import { gql, GraphQLClient } from "graphql-request";
+dotenv.config();
 
-console.log("Hello World!");
-console.log(process.env.SHOPIFY_API_KEY);
+const shop = process.env.SHOP;
+const accessToken = process.env.ACCESS_TOKEN;
+
+console.log("App started...", { shop, accessToken });
+
+if (shop && accessToken) {
+  const graphqlEndpoint = `https://${shop}.myshopify.com/admin/api/2022-07/graphql.json`;
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-Shopify-Access-Token': accessToken,
+  }
+  const client = new GraphQLClient(graphqlEndpoint);
+  // Read product's metafield.
+  const readProducts = gql `
+    {
+      products(first: 10) {
+        nodes {
+          id
+          metafields(first: 3) {
+            nodes {
+              id
+              key
+              value
+            }
+          }
+        }
+      }
+    }
+  `;
+  const products = await client.request(readProducts, {}, headers)
+    .then(async res => res?.products?.nodes);
+  console.log({ products });
+}
